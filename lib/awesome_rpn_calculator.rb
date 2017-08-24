76181f5d372e5bb075ab4ff6bd0b690360d2cadd
Tiarli Oliveira <tiarlioliveira@gmail.com>
2017-08-24 12:34:57 -0300

    feat(Stack): introduce Stack
    This commit introduces our new Stack class that will be
    responsible for managing our current processor stack values.

--- lib/awesome_rpn_calculator.rb
+++ lib/awesome_rpn_calculator.rb
@@ -5,6 +5,7 @@ require 'awesome_rpn_calculator/operator_loader'
 require 'awesome_rpn_calculator/processor_loader'
 require 'awesome_rpn_calculator/runner'
 require 'awesome_rpn_calculator/scanner'
+require 'awesome_rpn_calculator/stack'
 require 'awesome_rpn_calculator/version'
 require 'awesome_rpn_calculator/writer'
 
